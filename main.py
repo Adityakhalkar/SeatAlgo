ea4f18d7e5f54ff04a52390a49a33687a63c19ed
Aditya Khalkar <67951994+Adityakhalkar@users.noreply.github.com>
2024-03-11 08:47:05 +0000

    added content-1

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 # contents to be added here
 import streamlit as st
 st.title("Seat Algo")
+
+percentile = st.number_input("MHT-CET Percentile", value=None, placeholder="Enter your MHT-CET percentile")
+Merit = st.number_input("MHT-CET Merit No.", value=None, placeholder="Enter your Merit Number")
+
+options = st.multiselect(
+    "Enter your preferred branch",
+    ["Green", "Yellow", "Red", "Blue"]
+)
