e38996b014faf44b0df5d1e41cc98ff36b63cf1d
Aditya Khalkar <67951994+Adityakhalkar@users.noreply.github.com>
2024-04-16 23:41:16 +0530

    Create introduction.py

---
+++ pages/introduction.py
@@ -0,0 +1,12 @@
+st.image("seatalgo.png", use_column_width=True)
+# Description of the project
+    
+st.write("""
+
+        
+        Guess no more, which college you will get is predicted by our model. 
+        Check your Seat allocation status right now by clicking the below button.
+""")
+# Center align the button
+if st.button("Next"):
+    st.button.page_link("pages/main.py", label = "Next")
