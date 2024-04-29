d2806423a63097835b5f26f1b88e4b5656fbbae6
Aditya Khalkar <67951994+Adityakhalkar@users.noreply.github.com>
2024-04-30 00:57:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,152 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 
-# Load data
-df = pd.read_csv('final_df2.csv')
-institute_names = pd.read_csv('institute codes.csv')
+# Define introduction page
+def introduction_page():
+    st.markdown(
+    """
+    <style>
+    .st-emotion-cache-1v0mbdj.e115fcil1 {
+        display: block;
+        margin: 0 auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+    st.image("seatalgo.png", width=300)
+    st.header("""
+        Guess no more, :blue[college] selection is easy now.
+    """)
+    st.header("""
+    Check your :red[eligibility] status right now.
+    """)
 
-# Map institute codes to names
-institute_mapping = institute_names.set_index('code')['name'].to_dict()
-df['Institute Name'] = df['institute_code'].map(institute_mapping)
+    st.markdown(
+    """
+    <style>
+    .st-emotion-cache-q3uqly.ef3psqc13{
+        display: block;
+        margin: 0 auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+    st.divider()
 
-# Page title
-st.title("College Admission Predictor")
+    st.subheader("""
+     Click the button below to predict your college!
+    """)
+    st.divider()
+    if st.button("Next", type = 'primary'):
+        st.session_state.page = "main_project"
+        st.rerun()
+    st.divider()
+    st.write("""
+    Students often find it difficult to find colleges after their entrance exams, due to various factors. To overcome this issue we made SeatAlgo which helps students to find the colleges and branches they were unaware of. Potentially making students get better colleges.
+""")
+    st.divider()
 
-# Sidebar option for input method
-input_option = st.sidebar.radio("Choose Input Method", ("MHT-CET Percentile", "MHT-CET Merit No."))
 
-# Data preprocessing
-le = LabelEncoder()
-df['Category'] = le.fit_transform(df['Category'])
-df['branch_name'] = le.fit_transform(df['branch_name'])
-X = df[['MHT-CET Score', 'Category', 'branch_name']].values
-y = df['Institute Name'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Define main project page
+def main_project():
+    st.markdown(
+    """
+    <style>
+    .st-emotion-cache-1v0mbdj.e115fcil1 {
+        display: block;
+        margin: 0 auto;
+    }
+    p{
+    text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+    st.image("seatalgo.png", width=300)
+    df = pd.read_csv('final_df2.csv')
+    institute_names = pd.read_csv('institute codes.csv')
+    institute_mapping = institute_names.set_index('code')['name'].to_dict()
+    df['Institute Name'] = df['institute_code'].map(institute_mapping)
 
-# Train the model
-ada_clf = AdaBoostClassifier(n_estimators=100, random_state=42)
-ada_clf.fit(X_train, y_train)
+    input_option = st.sidebar.radio("Choose Input Method", ("Percentile", "Merit No."))
 
-# Input fields based on user choice
-if input_option == "MHT-CET Percentile":
-    percentile = st.sidebar.number_input("Enter MHT-CET Percentile", value=None, step=0.01)
-    Merit = None
-else:
-    Merit = st.sidebar.number_input("Enter MHT-CET Merit No.", value=None)
+    # Input fields based on user choice
+    if input_option == "Percentile":
+        percentile = st.sidebar.number_input("MHT-CET Percentile", value=None, placeholder="Enter your MHT-CET percentile")
+        Merit = None
+    else:
+        Merit = st.sidebar.number_input("MHT-CET Merit No.", value=None, placeholder="Enter your Merit Number")
+        percentile = None
 
-# Check if both inputs are provided
-if percentile is not None and Merit is not None:
-    st.error("Please provide only one input (Percentile or Merit No.)")
+    # Check if both inputs are provided
+    if percentile is not None and Merit is not None:
+        st.error("Please provide only one input (Percentile or Merit No.)")
 
-# Prediction based on user input
-if percentile is not None:
-    category = st.selectbox("Select Category: ", df['Category'].unique(), index=None)
-    branch = st.multiselect("Select Preferred Branches: ", np.sort(df['branch_name'].unique()), max_selections=5)
-    if st.button("Predict College", type="primary"):
-        input_data = np.array([[percentile, le.transform([category])[0], le.transform(branch)]])
-        college_prediction = ada_clf.predict(input_data)
-        st.success("Predicted College:")
-        st.write(college_prediction[0])
-elif Merit is not None:
-    category = st.selectbox("Select Category: ", df['Category'].unique(), index=None)
-    branch = st.multiselect("Select Preferred Branches: ", np.sort(df['branch_name'].unique()), max_selections=5)
-    if st.button("Predict College", type="primary"):
-        input_data = np.array([[Merit, le.transform([category])[0], le.transform(branch)]])
-        college_prediction = ada_clf.predict(input_data)
-        st.success("Predicted College:")
-        st.write(college_prediction[0])
-else:
-    st.warning("Please provide either MHT-CET percentile or Merit No.")
+    # Output based on user input
+    if percentile is not None:
+        category = st.selectbox(
+            "Select your Category: ",
+            df['Category'].unique(),
+            index=None,
+            placeholder="Select Category",
+        )
+        st.write("You selected:", category)
+        branch = st.multiselect(
+            "Enter your preferred branch",
+            np.sort(df['branch_name'].unique()),
+            max_selections=5
+        )
+        if st.button("Submit", type="primary"):
+            colleges = df['Institute Name'][(df['MHT-CET Score'] < percentile) & (df['Category'] == category) & (
+                        df['branch_name'].isin(branch))].unique()
+            if len(colleges) == 0:
+                st.write("No colleges available.")
+            else:
+                st.write("Allotable Colleges:")
+                for college in colleges:
+                    st.write(college)
+                    google_search_url = f"https://www.google.com/search?q={college.replace(' ', '+')}"
+                    st.info(f"[Show info about {college}]({google_search_url})")
+
+
+    elif Merit is not None:
+        category = st.selectbox(
+            "Select your Category: ",
+            df['Category'].unique(),
+            index=None,
+            placeholder="Select Category",
+        )
+        st.write("You selected:", category)
+        branch = st.multiselect(
+            "Enter your preferred branch",
+            np.sort(df['branch_name'].unique()),
+            max_selections=5
+        )
+        if st.button("Submit", type="primary"):
+            colleges = df['Institute Name'][(df['Merit No.'] < Merit) & (df['Category'] == category) & (
+                        df['branch_name'].isin(branch))].unique()
+            if len(colleges) == 0:
+                st.write("No colleges available.")
+            else:
+                st.write("Allotable Colleges:")
+                for college in colleges:
+                    st.write(college)
+                    google_search_url = f"https://www.google.com/search?q={college.replace(' ', '+')}"
+                    st.info(f"[Show info about {college}]({google_search_url})")
+
+    else:
+        st.write("Please provide either MHT-CET percentile or Merit No.")
+
+# Initialize session state
+if "page" not in st.session_state:
+    st.session_state.page = "introduction"
+
+# Render pages based on session state
+if st.session_state.page == "introduction":
+    introduction_page()
+elif st.session_state.page == "main_project":
+    main_project()
